80b2d6324c641bc13e0ad7b8a32c4401f349db0b
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-21 14:50:07 +0800

    Update project

--- handler.js
+++ handler.js
@@ -1,1 +1,9 @@
-console.log('handler js added');
+document.getElementById('btn-update-title')
+    .addEventListener('click', function updateTitle() {
+        // console.log('button click');
+        // document.body.style.backgroundColor = 'red';
+        let pageTitleElement = document.getElementById('page-title')
+        // console.log(pageTitleElement);
+        pageTitleElement.innerText = 'updated page title text';
+
+    })
